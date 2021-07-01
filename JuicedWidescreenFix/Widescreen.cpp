9a7c25659e68079ff40762c7d0e1587fa634d952
TFP-dev <pi4096c@protonmail.com>
2021-07-01 12:26:10 +0200

    Compatibility with patch v1.01

--- JuicedWidescreenFix/Widescreen.cpp
+++ JuicedWidescreenFix/Widescreen.cpp
@@ -52,8 +52,29 @@ void Widescreen::Initialise()
 
 void Widescreen::SetAspectRatio(float aspectRatio, float fov) 
 {
-    Memory::Patch<float>(0x72477C, aspectRatio);
+    int version = 0;
 
-    if (fov >= 1.0f || fov <= 2.0f)
-        Memory::Patch<float>(0x6FEAF0, fov);
+    if (*(float*)0x72477C == 1.15f)
+    {
+        Memory::Patch<float>(0x72477C, aspectRatio);
+        version = 100;
+    }
+    else if (*(float*)0x72778C == 1.15f)
+    {
+        Memory::Patch<float>(0x72778C, aspectRatio);
+        version = 101;
+    }
+    else
+    {
+        MessageBoxA(NULL, "Unsupported version detected!", "Juiced Widescreen Fix", MB_ICONINFORMATION);
+    }
+    
+
+    if (fov >= 1.0f && fov <= 2.0f)
+    {
+        if (version == 100)
+            Memory::Patch<float>(0x6FEAF0, fov);
+        else if (version == 101)
+            Memory::Patch<float>(0x701BE0, fov);
+    }
 }
